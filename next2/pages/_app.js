f4a4e1d09d0290402a952265ea1e02d4170d3c40
KryNik <miryasov_nikita@mail.ru>
2024-03-29 19:22:23 +0300

    Initial commit

--- next2/pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '../styles/globals.css'
-import 'react-awesome-slider/dist/styles.css';
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
